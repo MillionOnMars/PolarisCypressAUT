bb3076587870304f51d5026748902985deac5ad2
Wil <wilfred.escarda@gmail.com>
2025-07-09 12:31:39 +0800

    cleanup and lessen waiting time

--- cypress/support/data.js
+++ cypress/support/data.js
@@ -1,5 +1,3 @@
-const prime = ['2', '3', '5', '7', '11'];
-const capital = "Paris"
 const QA = ['Acme_QA','AMD_QA', 'AWS_QA', 'Broadcom_QA', 'ChadOrg_QA, Inc_QA', 'Cloudera_QA', 'Commvault_QA', 'DELL_QA', 'ElasticSearch_QA', 'Google_QA', 'GoTo_QA', 'Groq', 'HP_QA', 'HPE_QA', 'IBM_QA', 'Intel_QA', 'JulOrg_Test', 'Lattice_QA', 'Lenovo_QA', 'Logitech_QA', 'Marvell_QA', 'Microsoft_QA', 'NetApp_QA', 'Oracle_QA', 'PureStorage_QA', 'Qualcomm_QA', 'Salesforce_QA', 'ServiceNow_QA', 'Shure_QA', 'SmartSheet_QA', 'Synopsis_QA', 'Veeam_QA', 'Zoho_QA', 'Zoom_QA']; // Add your qas here
 
 const getRandomQA = (count) => {
@@ -10,7 +8,7 @@ const verifyQA = (qa) => {
     // Click the "New Chat" button
     cy.get("button[title='Open'] svg").click()
     cy.contains(qa, { timeout: 10000 }).click()
-    cy.wait(5000); // wait for 5 second
+    cy.wait(3000); // wait for 5 second
 
     cy.get("button[role='combobox']")
         .should('be.visible')
