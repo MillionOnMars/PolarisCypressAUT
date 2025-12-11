f36023fa9c247e3a38eef91057d4800812eafbb6
Wil <wilfred.escarda@gmail.com>
2025-12-11 08:45:42 +0800

    Fix research errors

--- cypress.config.js
+++ cypress.config.js
@@ -22,8 +22,8 @@ module.exports = defineConfig({
       'cypress/e2e/Organization.cy.js',
       // 'cypress/e2e/Reports.cy.js',
       // 'cypress/e2e/Analytics.cy.js',
-      'cypress/e2e/Migration.cy.js'
-      // 'cypress/e2e/Research_Engine.cy.js'
+      'cypress/e2e/Migration.cy.js',
+      'cypress/e2e/Research_Engine.cy.js'
     ],
     supportFile: 'cypress/support/index.js',
     downloadsFolder: 'cypress/downloads',
